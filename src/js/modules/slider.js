4b7fd14ef479e7ed7c8c8677b213fea0c4fddd96
Maxim <Rudeshkomaximsergeevich@gmail.com>
2020-07-28 23:10:17 +0300

    added slick slider for testimonials block

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,6 +1,32 @@
 $(document).ready(function () {
     $('.header-slider__inner').slick({
         arrows:false,
-        dots:true
+        dots:true,
+        autoplaySpeed:300,
+        autoplay:true
     })
+    $('.testimonials__contentbox').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.testimonials-slider__items',
+      });
+
+
+    $('.testimonials-slider__items').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.testimonials__contentbox',
+        arrows: false,
+        centerMode:true,
+        focusOnSelect: true,
+        draggable:false,
+        arrows:true
+        
+    }); 
+
+    
+
+
 });
